808f6dde309711baa22ac6ebe151ff68d7a92b3f
tibigg <tg4018@ic.ac.uk>
2022-02-26 23:32:48 +0000

    Moved openai key utility function out of the main program

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-import os
-from dotenv import load_dotenv
-
 from story_generators import *
-
-
-def load_openai_api_key():
-    load_dotenv()
-    openai.api_key = os.getenv('OPENAI_API_KEY')
-
+from util import load_openai_api_key
 
 if __name__ == '__main__':
     # Initialise OpenAi API Key (otherwise, generator is unusable)
     load_openai_api_key()
-
     response = children_story_generator(topics="Bee, Vampire")
     print(response)
